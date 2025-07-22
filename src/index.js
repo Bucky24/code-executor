5dbd4a1bea205c3e15b22adc9575afa6d46f07c5
Robbert Wijtman <robbert@getsquid.ai>
2025-07-22 13:21:50 -0400

    better executor

--- src/index.js
+++ src/index.js
@@ -2,14 +2,8 @@ const { Executor } = require("./executor");
 const { validate } = require("./validator");
 const { STRUCTURE_TYPE, COMPARISON_OPERATOR, MATH_OPERATOR } = require("./types");
 
-async function execute(code) {
-    const executor = new Executor(code);
-
-    await executor.execute();
-}
-
 module.exports = {
-    execute,
+    Executor,
     validate,
     STRUCTURE_TYPE,
     COMPARISON_OPERATOR,
